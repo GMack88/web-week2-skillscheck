9223bf0f0878d427e5f4445fd52887a417951f84
G Mack <mack.gerrit@gmail.com>
2019-08-09 15:21:30 -0700

    updated ode to code

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -4,7 +4,7 @@
 //////////////////STEP 1////////////////////
 //Create a variable called lovesCode and set it true or false (as a boolean) depending on if you love code or not. It's okay if you don't, yet
 
-var lovesCode === true;
+var lovesCode = true;
 
 //////////////////STEP 2////////////////////
 //Create a variable called age that is your age as a number
